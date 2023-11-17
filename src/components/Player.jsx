ef395162f45495c8b327085ff2c472cf46e5307a
Subhadeep Chakraborty <85724371+subhadeep-chakraborty@users.noreply.github.com>
2023-11-18 03:18:20 +0530

    two way binded input with Ref

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const playerName = useRef();
+
+  function handleSave() {
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value= "";
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text"  ref={playerName}/>
+        <button onClick={handleSave}>Set Name</button>
       </p>
     </section>
   );
